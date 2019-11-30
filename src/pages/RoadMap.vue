783ae698356127c87095994ef176408afbbda244
ringo2455 <iby2455@gmail.com>
2019-12-01 00:11:50 +0900

    data/react.js 추가

--- src/pages/RoadMap.vue
+++ src/pages/RoadMap.vue
@@ -1,43 +1,25 @@
 <template>
   <div>
     <div class="test">로드맵을 보여줄 페이지 입니다!!</div>
-    <mindmap
-      :nodes="nodes"
-      :connections="connections"
-      :editable="false"
-    />
+    <mindmap :nodes="nodes" :connections="connections" :editable="false" />
   </div>
 </template>
 
 <script>
+import data from "../data/react.js";
 
-    export default {
-        name: "roadmap_page",
-        data() {
-          return {
-            nodes: [
-              {
-                "text": "hello",
-                "url": "https://www.naver.com",
-                "category": "react"
-              },
-              {
-                "text": "world",
-                "url": "https://www.naver.com",
-                "category": "react"
-              }
-            ],
-            connections: [
-              {
-                "source": "hello",
-                "target": "world"
-              }
-            ],
-          };
-        }
-    }
+const { nodes, connections } = data;
+console.log(data);
+export default {
+  name: "roadmap_page",
+  data() {
+    return {
+      nodes,
+      connections
+    };
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
