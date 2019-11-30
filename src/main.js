07458192deb07e9cc228403feebb24aac9df9bc1
dongheon.shin <dev.plurly@gmail.com>
2019-11-30 20:21:22 +0900

    마인드맵 의존성 추가 및 데모 페이지 추가

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import VueMindmap from 'vue-mindmap'
+import 'vue-mindmap/dist/vue-mindmap.css'
+
 Vue.config.productionTip = false
+
+Vue.use(VueMindmap)
 
 /* eslint-disable no-new */
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import MainPage from '../pages/MainPage'
+import RoadMap from '../pages/RoadMap'
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    { path: '/', name: 'MainPage', component: MainPage }
+    { path: '/', name: 'MainPage', component: MainPage },
+    { path: '/roadmap', name: 'RoadMap', component: RoadMap }
   ]
 })
