aaddeb5dc6dcc04a57a197d3c31f97660a85bb95
haeeun <hegfs6327@naver.com>
2019-11-30 17:12:40 +0900

    프로젝트 기초

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="test">로드맵을 보여줄 페이지 입니다!!</div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "main_page"
+    }
+</script>
+
+<style scoped>
+
+</style>
